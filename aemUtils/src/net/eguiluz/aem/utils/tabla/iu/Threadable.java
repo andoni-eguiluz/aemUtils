c27186336189fb4ee1e376641d7a0b2ae58c91df
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2019-03-01 11:03:11 +0100

    Utilidades de tabla visual

---
+++ aemUtils/src/net/eguiluz/aem/utils/tabla/iu/Threadable.java
@@ -0,0 +1,7 @@
+package net.eguiluz.aem.utils.tabla.iu;
+
+/** Interfaz Runnable cuyo método run se ejecutará en un hilo independiente
+ * @author andoni.eguiluz @ ingenieria.deusto.es
+ */
+public interface Threadable extends Runnable {
+}
